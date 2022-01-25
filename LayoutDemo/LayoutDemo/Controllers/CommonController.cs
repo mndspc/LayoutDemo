9d9773149644886bac7101b3be6ba81c9511733a
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-25 18:48:16 +0530

    pushing new project

---
+++ LayoutDemo/LayoutDemo/Controllers/CommonController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutDemo.Controllers
+{
+    public class CommonController : Controller
+    {
+        // GET: Common
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUS()
+        {
+            return View();
+        }
+
+    }
+}
